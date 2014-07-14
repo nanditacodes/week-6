53d32557eb57d5ff5d5e6a05e6c3620fda2be807
Nandita Rajagopalan <nandita@Nanditas-MacBook-Pro.local>
2014-07-13 22:49:35 -0500

    removed extra files

--- jabber/app/models/assignment.rb
+++
@@ -1,6 +0,0 @@
-class Assignment < ActiveRecord::Base
-
-  mount_uploader :photo, PhotoUploader
-  belongs_to :user
-  
-end
--- jabber/app/views/assignments/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @assignment, :id, :name, :description, :user_id, :created_at, :updated_at
